9d7805af7b3a4cb3794e315b731a6f3ead63be5a
fumonster <617727365@qq.com>
2023-12-27 19:00:31 +0800

    feat: add basic webpack config

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 for practice
 
 1. 首先建立最基础的index.html文件
+2. 加入webpack依赖和配置
+   - 使用npm init初始化依赖
+   - 增加webpack配置
---
+++ src/index.js
@@ -0,0 +1,9 @@
+/**
+ * @file: entry file
+ * @author: huyanxin.owen
+ * @description：
+ * @date: 2023-12-27 16:56
+ * @copyright: bytedance 2023
+ */
+
+console.log('test pack');
